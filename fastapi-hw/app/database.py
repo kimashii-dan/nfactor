fb2146189907daadd9a32d16ad847f551220fde7
Daniyar <daniyarmunsyzbaev@gmail.com>
2025-06-09 23:06:14 +0500

    GA trigger

--- fastapi-hw/app/database.py
+++ fastapi-hw/app/database.py
@@ -13,9 +13,9 @@ if SQLALCHEMY_DATABASE_URL is None:
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
