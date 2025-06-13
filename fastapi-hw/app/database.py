67d6bffd447e08e058040adea953a06029a2d047
Daniyar <daniyarmunsyzbaev@gmail.com>
2025-06-13 09:19:19 +0500

    Minimax change

--- fastapi-hw/app/database.py
+++ fastapi-hw/app/database.py
@@ -13,6 +13,7 @@ if SQLALCHEMY_DATABASE_URL is None:
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def get_db():
